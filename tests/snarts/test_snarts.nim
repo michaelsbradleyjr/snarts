0edf273d5b6327b2d1b8f027c5c0c065686d72fa
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-06-26 01:03:17 -0500

    [wip] proper tests taking shape

--- tests/snarts/test_snarts.nim
+++ tests/snarts/test_snarts.nim
@@ -4,6 +4,24 @@ import pkg/unittest2
 # in tests `let` is preferred to `const` because the latter does not generate
 # coverate data
 
+func `==`[St, Ev, Dm, Em](a, b: StatechartNode[St, Ev, Dm, Em]): bool =
+  if a.kind != b.kind:
+    result = false
+  else:
+    case a.kind
+    of snkState:
+      result = (
+        (a.sId == b.sId) and
+        (a.sInitial == b.sInitial) and
+        (a.sChildren == b.sChildren))
+    else:
+      result = false
+
+func `==`[St, Ev, Dm, Em](a, b: Statechart[St, Ev, Dm, Em]): bool =
+  (a.scInitial == b.scInitial) and
+  (a.scName == b.scName) and
+  (a.scChildren == b.scChildren)
+
 suite "Templates":
   test "combinations of usage should not cause compile-time errors":
     type
@@ -11,7 +29,7 @@ suite "Templates":
         st1, st2
 
       Events = enum
-        evA, evB
+        evA
 
       Data = object
         id: States
@@ -19,153 +37,245 @@ suite "Templates":
       Event = object
         name: Events
 
+    # rewrite this to be in a `check:` block/s instead of `let`; lhs of `==`
+    # should be the template invocation as below and rhs should be
+    # expected-to-match invocation of `StatechartNode(kind: ..., ...)`
+
+    var chart = statechart(States, Events, Data, Event)
+
+    check:
+      statechart(States, Events, Data, Event) ==
+        Statechart[States, Events, Data, Event]()
+
+    check:
+      statechart(States, Events, Data, Event, []) ==
+        Statechart[States, Events, Data, Event](
+          scChildren: @[])
+      statechart(States, Events, Data, Event, children = []) ==
+        Statechart[States, Events, Data, Event](
+          scChildren: @[])
+    chart = statechart(States, Events, Data, Event): []
+    check:
+      chart ==
+        Statechart[States, Events, Data, Event](
+          scChildren: @[])
+
+    check:
+      statechart(States, Events, Data, Event, "test", []) ==
+        Statechart[States, Events, Data, Event](
+          scName: Opt.some "test",
+          scChildren: @[])
+      statechart(States, Events, Data, Event, "test", children = []) ==
+        Statechart[States, Events, Data, Event](
+          scName: Opt.some "test",
+          scChildren: @[])
+    chart = statechart(States, Events, Data, Event, "test"): []
+    check:
+      chart ==
+        Statechart[States, Events, Data, Event](
+          scName: Opt.some "test",
+          scChildren: @[])
+    check:
+      statechart(States, Events, Data, Event, name = "test", []) ==
+        Statechart[States, Events, Data, Event](
+          scName: Opt.some "test",
+          scChildren: @[])
+
     let
-      spec1a = statechart(States, Events, Data, Event, [])
-      spec1b = statechart(States, Events, Data, Event, children = [])
-      spec1c = statechart(States, Events, Data, Event): []
-      spec2aa = statechart(States, Events, Data, Event, "spec2aa", [])
-      spec2ab = statechart(States, Events, Data, Event, "spec2aa", children = [])
-      spec2ac = statechart(States, Events, Data, Event, "spec2aa"): []
-      spec2ba = statechart(States, Events, Data, Event, name = "spec2ab", [])
-      spec2bb = statechart(States, Events, Data, Event, name = "spec2ab", children = [])
-      spec2bc = statechart(States, Events, Data, Event, children = [], name = "spec2ab")
-      spec2bd = statechart(States, Events, Data, Event, name = "spec2ab"): []
-      spec2ca = statechart(States, Events, Data, Event, st1, [])
-      spec2cb = statechart(States, Events, Data, Event, st1, children = [])
-      spec2cc = statechart(States, Events, Data, Event, st1): []
-      spec2da = statechart(States, Events, Data, Event, initial = st1, [])
-      spec2db = statechart(States, Events, Data, Event, initial = st1, children = [])
-      spec2dc = statechart(States, Events, Data, Event, children = [], initial = st1)
-      spec2dd = statechart(States, Events, Data, Event, initial = st1): []
-      spec3aa = statechart(States, Events, Data, Event, "spec3", st1, [])
-      spec3ab = statechart(States, Events, Data, Event, "spec3", st1, children = [])
-      spec3ac = statechart(States, Events, Data, Event, "spec3", st1): []
-      spec3ba = statechart(States, Events, Data, Event, name = "spec3", st1, [])
-      spec3bb = statechart(States, Events, Data, Event, name = "spec3", st1, children = [])
-      spec3bc = statechart(States, Events, Data, Event, name = "spec3", st1): []
-      spec3ca = statechart(States, Events, Data, Event, "spec3", initial = st1, [])
-      spec3cb = statechart(States, Events, Data, Event, "spec3", initial = st1, children = [])
-      spec3cc = statechart(States, Events, Data, Event, "spec3", children = [], initial = st1)
-      spec3cd = statechart(States, Events, Data, Event, "spec3", initial = st1): []
-      spec3da = statechart(States, Events, Data, Event, name = "spec3", initial = st1, [])
-      spec3db = statechart(States, Events, Data, Event, name = "spec3", initial = st1, children = [])
-      spec3dc = statechart(States, Events, Data, Event, name = "spec3", children = [], initial = st1)
-      spec3dd = statechart(States, Events, Data, Event, initial = st1, name = "spec3", children = [])
-      spec3de = statechart(States, Events, Data, Event, initial = st1, children = [], name = "spec3")
-      spec3df = statechart(States, Events, Data, Event, children = [], name = "spec3", initial = st1)
-      spec3dg = statechart(States, Events, Data, Event, children = [], initial = st1, name = "spec3")
-      spec3dh = statechart(States, Events, Data, Event, name = "spec3", initial = st1): []
+      # chart0 = statechart(States, Events, Data, Event)
+
+      # chart1a = statechart(States, Events, Data, Event, [])
+      # chart1b = statechart(States, Events, Data, Event, children = [])
+      # chart1c = statechart(States, Events, Data, Event): []
+
+      # chart2a = statechart(States, Events, Data, Event, "test", [])
+      # chart2b = statechart(States, Events, Data, Event, "test", children = [])
+      # chart2c = statechart(States, Events, Data, Event, "test"): []
+      # chart2d = statechart(States, Events, Data, Event, name = "test", [])
+      chart2e = statechart(States, Events, Data, Event, name = "test", children = [])
+      chart2f = statechart(States, Events, Data, Event, children = [], name = "test")
+      chart2g = statechart(States, Events, Data, Event, name = "test"): []
+      chart2h = statechart(States, Events, Data, Event, st1, [])
+      chart2i = statechart(States, Events, Data, Event, st1, children = [])
+      chart2j = statechart(States, Events, Data, Event, st1): []
+      chart2k = statechart(States, Events, Data, Event, initial = st1, [])
+      chart2l = statechart(States, Events, Data, Event, initial = st1, children = [])
+      chart2m = statechart(States, Events, Data, Event, children = [], initial = st1)
+      chart2n = statechart(States, Events, Data, Event, initial = st1): []
+
+      chart3a = statechart(States, Events, Data, Event, "test", st1, [])
+      chart3b = statechart(States, Events, Data, Event, "test", st1, children = [])
+      chart3c = statechart(States, Events, Data, Event, "test", st1): []
+      chart3d = statechart(States, Events, Data, Event, name = "test", st1, [])
+      chart3e = statechart(States, Events, Data, Event, name = "test", st1, children = [])
+      chart3f = statechart(States, Events, Data, Event, name = "test", st1): []
+      chart3g = statechart(States, Events, Data, Event, "test", initial = st1, [])
+      chart3h = statechart(States, Events, Data, Event, "test", initial = st1, children = [])
+      chart3i = statechart(States, Events, Data, Event, "test", children = [], initial = st1)
+      chart3j = statechart(States, Events, Data, Event, "test", initial = st1): []
+      chart3k = statechart(States, Events, Data, Event, name = "test", initial = st1, [])
+      chart3l = statechart(States, Events, Data, Event, name = "test", initial = st1, children = [])
+      chart3m = statechart(States, Events, Data, Event, name = "test", children = [], initial = st1)
+      chart3n = statechart(States, Events, Data, Event, initial = st1, name = "test", children = [])
+      chart3o = statechart(States, Events, Data, Event, initial = st1, children = [], name = "test")
+      chart3p = statechart(States, Events, Data, Event, children = [], name = "test", initial = st1)
+      chart3q = statechart(States, Events, Data, Event, children = [], initial = st1, name = "test")
+      chart3r = statechart(States, Events, Data, Event, name = "test", initial = st1): []
 
       state0a = state(States, Events, Data, Event)
       state0b = anon(States, Events, Data, Event)
-      state1aa = state(States, Events, Data, Event, st1)
-      state1ab = state(States, Events, Data, Event, id = st1)
-      state1ba = anon(States, Events, Data, Event, st1)
-      state1bb = anon(States, Events, Data, Event, initial = st1)
-      state1ca = state(States, Events, Data, Event, [])
-      state1cb = state(States, Events, Data, Event, children = [])
-      state1cc = anon(States, Events, Data, Event, [])
-      state1cd = anon(States, Events, Data, Event, children = [])
 
-    # do these after all the variations for state, parallel, etc.
+    #   state1a = state(States, Events, Data, Event, st1)
+    #   state1b = state(States, Events, Data, Event, id = st1)
+    #   state1c = state(States, Events, Data, Event, [])
+    #   state1d = state(States, Events, Data, Event, children = [])
+    #   state1e = state(States, Events, Data, Event): []
+    #   state1f = anon(States, Events, Data, Event, st1)
+    #   state1g = anon(States, Events, Data, Event, initial = st2)
+    #   state1h = anon(States, Events, Data, Event, [])
+    #   state1i = anon(States, Events, Data, Event, children = [])
+    #   state1j = anon(States, Events, Data, Event): []
+
+    #   state2a = state(States, Events, Data, Event, st1, st2)
+    #   state2b = state(States, Events, Data, Event, id = st1, st2)
+    #   state2c = state(States, Events, Data, Event, id = st1, initial = st2)
+    #   state2d = state(States, Events, Data, Event, initial = st2, id = st1)
+    #   state2e = state(States, Events, Data, Event, st1, initial = st2)
+    #   state2f = state(States, Events, Data, Event, st1, [])
+    #   state2g = state(States, Events, Data, Event, st1): []
+    #   state2h = state(States, Events, Data, Event, id = st1, [])
+    #   state2i = state(States, Events, Data, Event, id = st1): []
+    #   state2j = state(States, Events, Data, Event, id = st1, children = [])
+    #   state2k = state(States, Events, Data, Event, children = [], id = st1)
+    #   state2l = state(States, Events, Data, Event, st1, children = [])
+    #   state2m = anon(States, Events, Data, Event, st1, [])
+    #   state2n = anon(States, Events, Data, Event, st1): []
+    #   state2o = anon(States, Events, Data, Event, initial = st2, [])
+    #   state2p = anon(States, Events, Data, Event, initial = st2): []
+    #   state2q = anon(States, Events, Data, Event, initial = st2, children = [])
+    #   state2r = anon(States, Events, Data, Event, children = [], initial = st2)
+    #   state2s = anon(States, Events, Data, Event, st2, children = [])
+
+    #   state3a = state(States, Events, Data, Event, st1, st2, [])
+    #   state3b = state(States, Events, Data, Event, st1, st2): []
+    #   state3c = state(States, Events, Data, Event, id = st1, st2, [])
+    #   state3d = state(States, Events, Data, Event, id = st1, st2): []
+    #   state3e = state(States, Events, Data, Event, id = st1, initial = st2, [])
+    #   state3f = state(States, Events, Data, Event, id = st1, initial = st2): []
+    #   state3g = state(States, Events, Data, Event, id = st1, initial = st2, children = [])
+    #   state3h = state(States, Events, Data, Event, id = st1, children = [], initial = st2)
+    #   state3i = state(States, Events, Data, Event, initial = st2, id = st1, children = [])
+    #   state3j = state(States, Events, Data, Event, initial = st2, children = [], id = st1)
+    #   state3k = state(States, Events, Data, Event, children = [], id = st1, initial = st2)
+    #   state3l = state(States, Events, Data, Event, children = [], initial = st2, id = st1)
+    #   state3m = state(States, Events, Data, Event, st1, initial = st2, [])
+    #   state3n = state(States, Events, Data, Event, st1, initial = st2): []
+    #   state3o = state(States, Events, Data, Event, st1, initial = st2, children = [])
+    #   state3p = state(States, Events, Data, Event, st1, children = [], initial = st2)
+    #   state3q = state(States, Events, Data, Event, st1, st2, children = [])
+
+    # --------------------------------------------------------------------------
+
+    # do the following after all the variations for state, parallel,
+    # etc. (though should probably be replaced with a fully macro-ized
+    # randomized setup)
+
     # for now can work out issues when fixup is in play for state in statechart
 
-    echo ""
-    echo spec1a
-    echo ""
-    echo spec1b
-    echo ""
-    echo spec1c
-    echo ""
-    echo spec2aa
-    echo ""
-    echo spec2ab
-    echo ""
-    echo spec2ac
-    echo ""
-    echo spec2ba
-    echo ""
-    echo spec2bb
-    echo ""
-    echo spec2bc
-    echo ""
-    echo spec2bd
-    echo ""
-    echo spec2ca
-    echo ""
-    echo spec2cb
-    echo ""
-    echo spec2cc
-    echo ""
-    echo spec2da
-    echo ""
-    echo spec2db
-    echo ""
-    echo spec2dc
-    echo ""
-    echo spec2dd
-    echo ""
-    echo spec3aa
-    echo ""
-    echo spec3ab
-    echo ""
-    echo spec3ac
-    echo ""
-    echo spec3ba
-    echo ""
-    echo spec3bb
-    echo ""
-    echo spec3bc
-    echo ""
-    echo spec3ca
-    echo ""
-    echo spec3cb
-    echo ""
-    echo spec3cc
-    echo ""
-    echo spec3cd
-    echo ""
-    echo spec3da
-    echo ""
-    echo spec3db
-    echo ""
-    echo spec3dc
-    echo ""
-    echo spec3dd
-    echo ""
-    echo spec3de
-    echo ""
-    echo spec3df
-    echo ""
-    echo spec3dg
-    echo ""
-    echo spec3dh
-    echo ""
-    echo state0a
-    echo ""
-    echo state0b
-    echo ""
-    echo state1aa
-    echo ""
-    echo state1ab
-    echo ""
-    echo state1ba
-    echo ""
-    echo state1bb
-    echo ""
-    echo state1ca
-    echo ""
-    echo state1cb
-    echo ""
-    echo state1cc
-    echo ""
-    echo state1cd
+    proc statePa(x: int): StatechartNode[States, Events, Data, Event] =
+      state(States, Events, Data, Event)
 
-    echo ""
+    let
+      stateVa = state(States, Events, Data, Event)
+
+    let
+      spec1 = statechart(States, Events, Data, Event, [
+        state
+      ])
 
     check:
-      statechart(States, Events, Data, Event, "", []).scName == Opt.none string
-      statechart(States, Events, Data, Event, "test", []).scName == Opt.some "test"
+      spec1 == Statechart[States, Events, Data, Event](
+        scChildren: @[StatechartNode[States, Events, Data, Event](
+          kind: snkState, sChildren: @[])])
+
+    let
+      spec2 = statechart(States, Events, Data, Event, [
+        # `snarts.state()` doesn't work, need to understand better 0 and 1+ arg
+        # forms of DotExpr:
+        # -----------------------------------------------------------------
+        # Bracket
+        #   Call
+        #     DotExpr
+        #       Ident "snarts"
+        #       Ident "state"
+        state()
+      ])
+
+    check:
+      spec2 == Statechart[States, Events, Data, Event](
+        scChildren: @[StatechartNode[States, Events, Data, Event](
+          kind: snkState, sChildren: @[])])
+
+    let
+      spec3 = statechart(States, Events, Data, Event, [
+        anon
+      ])
+
+    check:
+      spec3 == Statechart[States, Events, Data, Event](
+        scChildren: @[StatechartNode[States, Events, Data, Event](
+          kind: snkState, sChildren: @[])])
+
+    let
+      spec4 = statechart(States, Events, Data, Event, [
+        anon()
+      ])
+
+    check:
+      spec4 == Statechart[States, Events, Data, Event](
+        scChildren: @[StatechartNode[States, Events, Data, Event](
+          kind: snkState, sChildren: @[])])
+
+    let
+      spec5 = statechart(States, Events, Data, Event, [
+        # want to explore this in relation to outerscope having `state` (or
+        # `anon`, etc.) that's a proc and in collision with `snarts.state`, but
+        # see comments above re: DotExpr
+        statePa(123)
+      ])
+
+    check:
+      spec5 == Statechart[States, Events, Data, Event](
+        scChildren: @[StatechartNode[States, Events, Data, Event](
+          kind: snkState, sChildren: @[])])
+
+    let
+      spec6 = statechart(States, Events, Data, Event, [
+        # want to explore this in relation to outerscope having `state` (or
+        # `anon`, etc.) that's a value and in collision with `snarts.state`,
+        # but see comments above re: DotExpr
+        stateVa
+      ])
+
+    check:
+      spec6 == Statechart[States, Events, Data, Event](
+        scChildren: @[StatechartNode[States, Events, Data, Event](
+          kind: snkState, sChildren: @[])])
+
+    # --------------------------------------------------------------------------
+
+    # re-enable this check after working out all the above; leave it disabled
+    # for now because with `-d:debugMacros` the output resulting from the code
+    # below makes it more difficult to sort out what errored and why (i.e. as
+    # the DSL front-end is sorted out)
+
+    # check:
+    #   statechart(States, Events, Data, Event, "", []) ==
+    #     Statechart[States, Events, Data, Event]()
+    #   statechart(States, Events, Data, Event, "", []) ==
+    #     Statechart[States, Events, Data, Event](scName: Opt.none string)
+    #   statechart(States, Events, Data, Event, "test", []) ==
+    #     Statechart[States, Events, Data, Event](scName: Opt.some "test")
 
 suite "Validation":
   test "statechart must have one or more children":
