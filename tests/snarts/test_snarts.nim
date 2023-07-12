9275361eb3c83034a9bfd580b8d7a19a08867dd9
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2023-07-12 10:32:37 -0500

    [wip] the start of proper tests re: latest strategy

--- tests/snarts/test_snarts.nim
+++ tests/snarts/test_snarts.nim
@@ -29,7 +29,7 @@ suite "DSL front-end":
         st1, st2
 
       Events = enum
-        evA
+        evA, evB
 
       Data = object
         id: States
@@ -37,33 +37,96 @@ suite "DSL front-end":
       Event = object
         name: Events
 
-    # should turn the `debugEcho`s into `check: ...` where rhs is equivalent
-    # expected StateChart/Node instance composed with raw constructors:
-    # `Statechart[States, Events, Data, Event](...)`
+    let
+      aChild = state(States, Events, Data, Event)
+      aEvent = evA
+      aName = "test"
+      aState = st1
 
-    var chart =
-      statechart(
-        States, Events, Data, Event)
+    proc aCall(): auto =
+      aChild
 
-    debugEcho ""
-    debugEcho chart
+    var
+      chart =
+        statechart(States, Events, Data, Event)
+
+      chexp =
+        Statechart[States, Events, Data, Event]()
+
+    check: chart == chexp
 
     chart =
-      statechart(
-        States, Events, Data, Event,
-        "test",
-        st1,
-        state(),
-        state(),
-        state(),
-        state(),
-        state()
-      )
+      statechart(States, Events, Data, Event,
+        "test")
 
-    debugEcho ""
-    debugEcho chart
+    chexp = Statechart[States, Events, Data, Event](
+      scInitial: Opt.none States,
+      scName: Opt.some "test",
+      scChildren: @[])
 
-    debugEcho ""
+    check: chart == chexp
+
+    chart =
+      statechart(States, Events, Data, Event,
+        name = "test")
+
+    check: chart == chexp
+
+    chart =
+      statechart(States, Events, Data, Event,
+        st1)
+
+    chexp = Statechart[States, Events, Data, Event](
+      scInitial: Opt.some st1,
+      scName: Opt.none string,
+      scChildren: @[])
+
+    check: chart == chexp
+
+    chart =
+      statechart(States, Events, Data, Event,
+        initial = st1)
+
+    check: chart == chexp
+
+    chart =
+      statechart(States, Events, Data, Event,
+        [])
+
+    chexp = Statechart[States, Events, Data, Event](
+      scInitial: Opt.none States,
+      scName: Opt.none string,
+      scChildren: @[])
+
+    check: chart == chexp
+
+    chart =
+      statechart(States, Events, Data, Event,
+        children = [])
+
+    check: chart == chexp
+
+    chart =
+      statechart(States, Events, Data, Event,
+        aName,
+        aState,
+        [aCall(), aChild])
+
+    chexp = Statechart[States, Events, Data, Event](
+      scInitial: Opt.some aState,
+      scName: Opt.some aName,
+      scChildren: @[aCall(), aChild])
+
+    check: chart == chexp
+
+    chart =
+      statechart(States, Events, Data, Event,
+        initial = aState,
+        name = aName,
+        aCall(),
+        aChild)
+
+    check: chart == chexp
 
 suite "Validation":
   test "statechart must have one or more children":
